e7c86883470f304dbab57511d4cd3c66bdf67cb6
Robert Jamison <65142411+robertjamison@users.noreply.github.com>
2024-08-13 23:28:00 -0400

    init v0.1.5

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ buildscript {
 
 allprojects {
     group = "app.lexilabs.basic"
-    version = "0.1.4"
+    version = "0.1.5"
 
     apply(plugin = "org.jetbrains.dokka")
     apply(plugin = "maven-publish")
