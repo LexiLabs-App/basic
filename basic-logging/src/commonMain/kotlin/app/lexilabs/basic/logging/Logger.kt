48591e58eece6b158a445f50f7e67f7abc39ccab
Robert Jamison <65142411+robertjamison@users.noreply.github.com>
2024-08-13 22:28:10 -0400

    Updated comments for dokka

--- basic-logging/src/commonMain/kotlin/app/lexilabs/basic/logging/Logger.kt
+++ basic-logging/src/commonMain/kotlin/app/lexilabs/basic/logging/Logger.kt
@@ -1,5 +1,7 @@
 package app.lexilabs.basic.logging
 
+import app.lexilabs.basic.logging.Log.wtf
+
 /**
  * A common interface to enable multiplatform logging.
  *
@@ -7,10 +9,130 @@ package app.lexilabs.basic.logging
  *
  */
 public interface Logger {
+
+    /**
+     * When overridden, this should extend a logging function
+     *
+     * Send logcat the least important message possible.
+     *
+     * @param[tag] typically the name of the parent class.
+     * @param[message] whatever string of information you'd like to pass.
+     *
+     * ```kotlin
+     * class RandomClass() {
+     *
+     *   val tag = "RandomClass"
+     *
+     *   fun test() {
+     *     Log.v(tag, "it works!")
+     *   }
+     * }
+     * ```
+     */
     public fun v(tag: String, message: String)
+
+    /**
+     * When overridden, this should extend a logging function
+     *
+     * Send logcat a message that indicates where a fault is occurring
+     *
+     * @param[tag] typically the name of the parent class.
+     * @param[message] whatever string of information you'd like to pass.
+     *
+     * ```kotlin
+     * class RandomClass() {
+     *
+     *   val tag = "RandomClass"
+     *
+     *   fun test() {
+     *     Log.d(tag, "it works!")
+     *   }
+     * }
+     * ```
+     */
     public fun d(tag: String, message: String)
+
+    /**
+     * When overridden, this should extend a logging function
+     *
+     * Send logcat a routine message that you want the developer to see.
+     *
+     * @param[tag] typically the name of the parent class.
+     * @param[message] whatever string of information you'd like to pass.
+     *
+     * ```kotlin
+     * class RandomClass() {
+     *
+     *   val tag = "RandomClass"
+     *
+     *   fun test() {
+     *     Log.i(tag, "it works!")
+     *   }
+     * }
+     * ```
+     */
     public fun i(tag: String, message: String)
+
+    /**
+     * When overridden, this should extend a logging function
+     *
+     * Send logcat a warning message that indicates depreciated features or risky app behaviour
+     *
+     * @param[tag] typically the name of the parent class.
+     * @param[message] whatever string of information you'd like to pass.
+     *
+     * ```kotlin
+     * class RandomClass() {
+     *
+     *   val tag = "RandomClass"
+     *
+     *   fun test() {
+     *     Log.w(tag, "it works!")
+     *   }
+     * }
+     * ```
+     */
     public fun w(tag: String, message: String)
+
+    /**
+     * When overridden, this should extend a logging function
+     *
+     * Send logcat an error message that aids the developer in troubleshooting
+     *
+     * @param[tag] typically the name of the parent class.
+     * @param[message] whatever string of information you'd like to pass.
+     *
+     * ```kotlin
+     * class RandomClass() {
+     *
+     *   val tag = "RandomClass"
+     *
+     *   fun test() {
+     *     Log.e(tag, "it did not work!")
+     *   }
+     * }
+     * ```
+     */
     public fun e(tag: String, message: String)
+
+    /**
+     * When overridden, this should extend a logging function
+     *
+     * [wtf] (A.K.A. What the F***) Something so inexplicably impossible has occurred that you want to immediately draw as much attention to the error as possible because you don't want to get fired.
+     *
+     * @param[tag] typically the name of the parent class.
+     * @param[message] whatever string of information you'd like to pass.
+     *
+     * ```kotlin
+     * class RandomClass() {
+     *
+     *   val tag = "RandomClass"
+     *
+     *   fun thisWillNeverHappen() {
+     *     Log.wtf(tag, "BEFORE DEBUG: call that dude from LinkedIn back and accept the job.")
+     *   }
+     * }
+     * ```
+     */
     public fun wtf(tag: String, message: String)
 }
--- basic-sound/src/commonMain/kotlin/app/lexilabs/basic/sound/AudioState.kt
+++ basic-sound/src/commonMain/kotlin/app/lexilabs/basic/sound/AudioState.kt
@@ -1,6 +1,5 @@
 package app.lexilabs.basic.sound
 
-@ExperimentalBasicSound
 /**
  * @property[NONE] indicates merely an initial state where no audio has loaded
  * @property[LOADING] an audio file is being loaded
@@ -9,11 +8,36 @@ package app.lexilabs.basic.sound
  * @property[PAUSED] player has stopped but retained its progress / timecode
  * @property[ERROR] Something unexpected occurred. Error will be printed to logs
  */
+@ExperimentalBasicSound
 public sealed class AudioState {
+
+    /**
+     * @property[NONE] Indicates merely an initial state where no audio has loaded
+     */
     public data object NONE: AudioState()
+
+    /**
+     * @property[LOADING] an audio file is being loaded
+     */
     public data object LOADING : AudioState()
+
+    /**
+     * @property[READY] sound is ready to be played
+     */
     public data object READY : AudioState()
+
+    /**
+     * @property[PLAYING] player is currently playing audio, even if not audible
+     */
     public data object PLAYING : AudioState()
+
+    /**
+     * @property[PAUSED] player has stopped but retained its progress / timecode
+     */
     public data object PAUSED : AudioState()
+
+    /**
+     * @property[ERROR] Something unexpected occurred. Error will be printed to logs
+     */
     public data class ERROR(val message: String) : AudioState()
 }
