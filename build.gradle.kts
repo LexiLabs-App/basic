f72f35ead66e2adb27f28ee178b41e662cf47d62
Robert Jamison <65142411+robertjamison@users.noreply.github.com>
2024-09-09 22:29:19 -0400

    Init v0.1.8 Increased dependabot.yml frequency

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ buildscript {
 
 allprojects {
     group = "app.lexilabs.basic"
-    version = "0.1.7"
+    version = "0.1.8"
 
     apply(plugin = "org.jetbrains.dokka")
     apply(plugin = "maven-publish")
