d7b18e4de1e5fa5f80381e73f73176a74f32f57c
Robert Jamison <65142411+robertjamison@users.noreply.github.com>
2024-10-15 22:27:19 -0400

    Basic-Ads is WORKING on iOS!!!!

--- basic-ads/src/iosMain/kotlin/app/lexilabs/basic/ads/Composables.kt
+++ basic-ads/src/iosMain/kotlin/app/lexilabs/basic/ads/Composables.kt
@@ -1,35 +1,36 @@
 package app.lexilabs.basic.ads
 
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.UIKitView
 import app.lexilabs.basic.logging.Log
 import cocoapods.Google_Mobile_Ads_SDK.GADBannerView
 import cocoapods.Google_Mobile_Ads_SDK.GADRequest
 import kotlinx.cinterop.ExperimentalForeignApi
-import kotlinx.cinterop.readValue
 import platform.UIKit.UIApplication
-import platform.UIKit.UIView
 
 @OptIn(ExperimentalForeignApi::class)
 @Composable
 public actual fun BannerAd(adId: String, adSize: AdSize) {
-    Log.d("BannerAd", "starting")
+    Log.d("BannerAd", "composable:starting")
 //    DisposableEffect(Unit) {
 //        onDispose { TODO() }
 //    }
     UIKitView(
         factory = {
-            val bannerView = GADBannerView(adSize.toIos().readValue()).apply {
+            val viewController = UIApplication.sharedApplication.keyWindow?.rootViewController
+            checkNotNull(viewController) { "Root ViewController is null" }
+
+            val bannerView = GADBannerView(adSize.toCGRectCValue()).apply {
                 adUnitID = adId
-                rootViewController = UIApplication.sharedApplication.keyWindow?.rootViewController
-                if (rootViewController == null) {
-                    Log.d("BannerAd","rootController=null")
-                }
+                this.rootViewController = viewController
                 loadRequest(GADRequest())
             }
-            Log.d("BannerAd","finished")
             bannerView
         },
+        modifier = Modifier.size(width = adSize.width.dp, height = adSize.height.dp)
     )
 }
 
