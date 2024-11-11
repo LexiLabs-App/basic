80a38589226eada4cc1f02060ec0449d23b176d9
Robert Jamison <65142411+robertjamison@users.noreply.github.com>
2024-11-10 21:44:24 -0500

    Fixed a small error related to expect/actual

--- basic-ads/src/iosMain/kotlin/app/lexilabs/basic/ads/AdUnitId.kt
+++ basic-ads/src/iosMain/kotlin/app/lexilabs/basic/ads/AdUnitId.kt
@@ -1,6 +1,6 @@
 package app.lexilabs.basic.ads
 
-public object AdUnitId {
+public actual object AdUnitId {
     public actual fun autoSelect(androidAdUnitId: String?, iosAdUnitId: String?, jsAdUnitId: String?): String? {
         return iosAdUnitId
     }
