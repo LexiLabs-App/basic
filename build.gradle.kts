cc1cfa74b790eeae9c9f1e7b586d2e1bb5004787
Robert Jamison <65142411+robertjamison@users.noreply.github.com>
2025-01-31 14:41:17 -0500

    * Added onLoad callback for BannerAd, InterstitialAd, RewardedAd, and RewardedInterstitialAd @composables * Updated documentation

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,7 +24,7 @@ dependencies {
 
 allprojects {
     group = "app.lexilabs.basic"
-    version = "0.2.4"
+    version = "0.2.5-beta01"
 
     apply(plugin = "org.jetbrains.dokka")
     apply(plugin = "maven-publish")
