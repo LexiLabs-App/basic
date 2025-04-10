b912baf11b149c579042f88d4bc793f33af8c491
Robert Jamison <65142411+robertjamison@users.noreply.github.com>
2025-04-10 12:10:06 -0400

    Archive (#67)
    * archive init
    
    * removed dependabot.yml
    removed FUNDING.yml
    
    * removed .gitignore
    changed note to span instead of h2
    
    * removed unnecessary structures
    
    * Using caution instead of span
    
    * Changed terminology to redirect users to other repos

--- basic-sound/src/commonMain/kotlin/app/lexilabs/basic/sound/AudioByteBuilder.kt
+++
@@ -1,7 +0,0 @@
-package app.lexilabs.basic.sound
-
-public interface AudioByteBuilder {
-    public fun load(context: Any, localPath: String): Any
-    public fun play(item: Any)
-    public fun release()
-}
